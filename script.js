5d68cef7d959f2082e81f7d9c9a10121c8acf57c
Roy <89213640+p2reneker25@users.noreply.github.com>
2022-02-04 19:03:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/*Variables*/
+const seaLevelRate = 0.14;
+/*Functions*/
+function convertAddressToLocation(address)
+{
+  //convert address parameter to lat/long
+  return latitude, longitude;
+}
+function getElevation(lat, long)
+{
+  //convert lat and long to elevation above sea level
+}
+function calculateSeaLevel(elevation)
+{
+  //take elevation and find out how long it takes for sea to rise at global constant of 0.14
+}
+
